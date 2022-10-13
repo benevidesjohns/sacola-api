70239c814b9a162e5eb15597a4b5c5f9d4d9fd73
sanddlekfx <sanddlexown@hotmail.com>
2022-10-12 22:37:50 -0300

    feat: adicionadas configurações do swagger ui

--- src/main/java/me/dio/sacola/resource/SacolaResource.java
+++ src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -1,5 +1,6 @@
 package me.dio.sacola.resource;
 
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import me.dio.sacola.model.Item;
 import me.dio.sacola.model.Sacola;
@@ -7,6 +8,7 @@ import me.dio.sacola.resource.dto.ItemDto;
 import me.dio.sacola.service.SacolaService;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value="/ifood/devweek/sacolas")
 @RestController
 @RequestMapping("/ifood-devweek/sacolas")
 @RequiredArgsConstructor
